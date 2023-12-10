675841ba7284a1762484ec319b6bd81785c0db29
Belloshehu <belloshehu1@gmail.com>
2023-12-10 10:27:05 +0100

    Add sorting, pagination and filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,27 @@
-const feed = (parent, args, context) => {
-  return context.prisma.link.findMany();
+const feed = async (parent, args, context, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          {
+            url: { contains: args.filter },
+          },
+          {
+            description: { contains: args.filter },
+          },
+        ],
+      }
+    : {};
+  const count = await context.prisma.link.count({ where });
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+  return {
+    links,
+    count,
+  };
 };
 
 module.exports = {
